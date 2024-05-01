0808658cb300c9a9866a42318f1f7784d0dbba73
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-05-01 09:44:08 +0800

    added buy counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import { useNavigate } from "react-router-dom"
 
 function App() {
   const navigate = useNavigate()
+  const [count, setCount] = useState(0)
+  const [divis, setdivis] = useState(0)
+  const [oneTime, setOneTime] = useState(false)
   function navigateToBuy() {
     navigate("buy")
   }
   const [showBuy, setShowBuy] = useState(true)
   function toggleText() {
     setShowBuy(!showBuy)
+    setOneTime(true)
+    setdivis(divis + 1)
+    if (divis % 2 == 0) {
+      setCount(count + 1)
+    }
   }
   return (
     <>
@@ -17,14 +25,17 @@ function App() {
         <h2 className="text-2xl">Makes Life Premium</h2>
         <img src="/printer.svg" width={256} />
         <button onClick={toggleText} className="flex bg-black text-white rounded-full px-4 py-2 items-center justify-center h-11 w-fit">
-          
-          {showBuy? 
-         "Buy Now"          
-          :
-"<3 You just bought a Printerium printer!!!!"
-        }
-          </button>
+
+          {showBuy ?
+            "Buy Now"
+            :
+            "<3 You just bought a Printerium printer!!!!"
+          }
+        </button>
+        {oneTime ? <span>Printerium printers bought: {count}</span> : <></>}
+
       </div>
+
     </>
   )
 }
